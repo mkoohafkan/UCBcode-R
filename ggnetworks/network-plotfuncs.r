244b189a115dff11d3b0fcfa78b2753885e43291
mkoohafkan <michael.koohafkan@gmail.com>
2013-07-16 19:49:08 -0700

    overhauled network-plotfuncs.r to work with data frames rather than adjacency matrices

--- ggnetworks/network-plotfuncs.r
+++ ggnetworks/network-plotfuncs.r
@@ -1,42 +1,40 @@
-connect_adjacent <- function(locs, adjacency, threshold=0, directional=FALSE, ...){
-	# takes the adjacency matrix and the node coordinates to plot line segments
-	# adjacency = square floating point matrix of length(locs) 
-	# 	          element values are strength of connection
-	# locs = data.frame(id, x, y)
-	# threshold = 
-	# ... are arguments for arrow() function
-	require(grid)
-	require(ggplot2)
+plot_graph <- function(vertices, edges, directional=FALSE, weightcol=NULL, threshold=0, ...){
+# vertices = vertices with location information
+#   names(vertices) = c('id', 'x', 'y')
+# edges = edge dataframe
+#   names(edges) = c('from', 'to', ...)
+#  weightcol = name of column (if any) to use for edge weights
+# threshold = minimum edge weight required to be plotted. 
+#			  Ignored if weightcol = NULL
+# ... = optional parameters for arrow(). 
+#       ignored if directional = FALSE
 	
-	xs <- rep(NA, sum(adjacency))
+	# get coordinates of edge start and end points
+	xs <- rep(NA, nrow(edges))
 	ys <- xs
 	xends <- xs
 	yends <- xs
-	strengths <- xs
-	segs<- vector("list", sum(adjacency) )
-	ind <- 0	# loop index
-	for(i in seq(1, dim(adjacency)[1], 1)){
-		for(j in seq(1, dim(adjacency)[2], 1)){
-			if(adjacency[i, j] > threshold){
-				ind <- ind + 1
-				xs[[ind]] <- locs[['projx']][i]
-				ys[[ind]] <- locs[['projy']][i]
-				xends[[ind]] <- locs[['projx']][j]
-				yends[[ind]] <- locs[['projy']][j]
-				strengths[[ind]] <- adjacency[i, j]
-			}
-		}
+	for(i in seq(along=xs)){
+		xs[i] <- vertices[['x']][vertices[['id']]==edges[['from']][i] ]
+		ys[i] <- vertices[['y']][vertices[['id']]==edges[['from']][i] ]
+		xends[i] <- vertices[['x']][vertices[['id']]==edges[['to']][i] ]
+		yends[i] <- vertices[['y']][vertices[['id']]==edges[['to']][i] ]
 	}
-	segdat <- data.frame(x=xs, y=ys, xend=xends, yend=yends, strength=strengths)
+	segdat <- data.frame(x=xs, y=ys, xend=xends, yend=yends)
+	if(!is.null(weightcol)){
+		segdat['weight'] <- edges[[weightcol]]
+		segdat <- segdat[!(segdat$weight < threshold),]
+	} else {
+		segdat['weight'] <- 1
+	}
 	if(directional){
-		segs <- geom_segment(data=segdat, aes(x=x, y=y, 
-		                                      xend=xend, yend=yend, 
-							 				  size=factor(strength)), 
+		require(grid)
+		segs <- geom_segment(data=segdat, aes(x=x, y=y, xend=xend, yend=yend, 
+											  color=weight), 
 							 arrow=arrow(...))		
 	} else {
-		segs <- geom_segment(data=segdat, aes(x=x, y=y, 
-		                                      xend=xend, yend=yend, 
-											  size=factor(strength)))
+		segs <- geom_segment(data=segdat, aes(x=x, y=y, xend=xend, yend=yend, 
+										  color=weight))
 	}
 	return(segs)
 }
