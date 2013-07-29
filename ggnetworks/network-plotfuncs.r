030cc55e379c9239feb35f6ff299fb176a01bc98
mkoohafkan <michael.koohafkan@gmail.com>
2013-07-28 20:59:46 -0700

    update is work in progress. network-plotfuncs.r now includes a fortify_edges() function and a helper function graph_to_dataframe() which is probably redundant

--- ggnetworks/network-plotfuncs.r
+++ ggnetworks/network-plotfuncs.r
@@ -1,3 +1,24 @@
+graph_to_dataframe(graphobj){
+	# split graphobj into vertices and edges
+	vertices <- get.data.frame(graphobj, what="vertices")
+	edges <- get.data.frame(graphobj, what="edges")
+	return(list(edges=edges, vertices=vertices))
+}
+
+fortify_edges <- function(edges, vertices){
+	xs <- rep(NA, nrow(edges))
+	ys <- xs
+	xends <- xs
+	yends <- xs
+	for(i in seq(along=xs)){
+		xs[i] <- vertices[['x']][vertices[['id']]==edges[['from']][i] ]
+		ys[i] <- vertices[['y']][vertices[['id']]==edges[['from']][i] ]
+		xends[i] <- vertices[['x']][vertices[['id']]==edges[['to']][i] ]
+		yends[i] <- vertices[['y']][vertices[['id']]==edges[['to']][i] ]
+	}
+		return(data.frame(edges, x=xs, y=ys, xend=xends, yend=yends))
+}
+
 plot_graph <- function(vertices, edges, directional=FALSE, weightcol=NULL, threshold=0, ...){
 # vertices = vertices with location information
 #   names(vertices) = c('id', 'x', 'y')
@@ -10,16 +31,6 @@ plot_graph <- function(vertices, edges, directional=FALSE, weightcol=NULL, thres
 #       ignored if directional = FALSE
 	
 	# get coordinates of edge start and end points
-	xs <- rep(NA, nrow(edges))
-	ys <- xs
-	xends <- xs
-	yends <- xs
-	for(i in seq(along=xs)){
-		xs[i] <- vertices[['x']][vertices[['id']]==edges[['from']][i] ]
-		ys[i] <- vertices[['y']][vertices[['id']]==edges[['from']][i] ]
-		xends[i] <- vertices[['x']][vertices[['id']]==edges[['to']][i] ]
-		yends[i] <- vertices[['y']][vertices[['id']]==edges[['to']][i] ]
-	}
 	segdat <- data.frame(x=xs, y=ys, xend=xends, yend=yends)
 	if(!is.null(weightcol)){
 		segdat['weight'] <- edges[[weightcol]]
@@ -27,6 +38,7 @@ plot_graph <- function(vertices, edges, directional=FALSE, weightcol=NULL, thres
 	} else {
 		segdat['weight'] <- 1
 	}
+	segdat['weight2'] <- edges[['sync']]
 	if(directional){
 		require(grid)
 		segs <- geom_segment(data=segdat, aes(x=x, y=y, xend=xend, yend=yend, 
@@ -34,7 +46,7 @@ plot_graph <- function(vertices, edges, directional=FALSE, weightcol=NULL, thres
 							 arrow=arrow(...))		
 	} else {
 		segs <- geom_segment(data=segdat, aes(x=x, y=y, xend=xend, yend=yend, 
-										  color=weight))
+										  color=weight, size=sync))
 	}
 	return(segs)
 }
