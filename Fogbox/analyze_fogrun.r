9e75e504ce6d54234471b4862c67f8c2fd6e9c30
mkoohafkan <michael.koohafkan@gmail.com>
2013-08-28 21:37:40 -0700

    major overhaul of analyze_fogrun.r with improved error propagation

--- Fogbox/analyze_fogrun.r
+++ Fogbox/analyze_fogrun.r
@@ -1,45 +1,75 @@
 # analyze_fogrun.r
-analyze_fogrun <- function(procdata, calibrationstandard=data.frame(avg=5.33, stderr=0)){
+analyze_fogrun <- function(procdata, calibrationstandard=data.frame(avg=5.33, stdev=0)){
 # procdata is a list of objects produced by process_fogrun
 # calibrationstandard = the mean and standard error of the
 #   reference mass used for calibrating load cell
-#   names(calibrationstandard) = c('avg', 'stderr')
-
+#
 ###
   # helper functions
   get_species <- function(s){
     # return the species, assuming naming convention is species followed
-	# by a numeric indicating the trial, e.g. blackoak1, blueoak3rep2, etc.
+    # by a numeric indicating the trial, e.g. blackoak1, blueoak3rep2, etc.
 	n <- 1
-	while(is.na(as.numeric(s[n])))
+	while(is.na(as.numeric(substr(s, n, n))))
 	  n <- n + 1
 	return(substr(s, 1, n))
   }
-  summarize_load <- function(d, usecorrected=FALSE){
-    # get the mean and standard deviation of a group of 
-	# SEmV or CSEmV measurements
-    if(usecorrected){ # tells whether to use CSEmV or SEmV 
-      data.frame(timestamp=max(d$timestamp), avg=mean(d$CSEmV), 
-	             stdev=sd(d$CSEmV))
-    } else {
-      data.frame(timestamp=max(d$timestamp), avg=mean(d$SEmV), 
-	             stdev=sd(d$SEmV))
-    }
+  calc_with_uncertainty <- function(expr, d, proptype='stat'){
+    # expr is an expression, e.g. expression(a - b)
+	# d is a dataframe with columns corresponding to
+	#   expression (e.g. names(d) = c('a', 'b')).
+	#   first row contains means, second row contains sd
+	r <- propagate(expr, d, type=proptype)$summary
+	avg <- r$Prop[1]
+	stdev <- r$Prop[2]
+	return(data.frame(avg=avg, stdev=stdev))
   }
-  calc_drift <- function(startvolt, endvolt){
-    # returns a vector of predicted change in baseline voltage
-	# for each timestamp, assuming a linear 
-    times <- seq(startvolt$timestamp, endvolt$timestamp, by=15)
-    change <- c(0, seq(1, length(times) - 1)*(endvolt$avg - startvolt$avg)
-                                            /(length(times) - 1))
-    return(data.frame(timestamp=times, driftmV=change))
+  stats_with_uncertainty <- function(d){
+    # assumes d is a dataframe of measurements with two columns
+    # first column is means, second column is standard deviations 
+    widedat <- data.frame(dummy=c(NA, NA)) # need a dummy column for cbind
+    nobs <- nrow(d)
+    for(i in seq(nobs))
+      widedat <- cbind(widedat, data.frame(m=c(d[[1]][nobs], d[[2]][nobs])))
+	widedat['dummy'] <- NULL # remove the dummy column
+	names(widedat) <- paste('m', seq(nobs), sep='')
+    widedat['nobs'] <- c(nobs, 0)
+	expr <- parse(text=paste('(', paste(names(widedat), collapse='+'), ')/nobs'))   
+    return(calc_with_uncertainty(expr, widedat))
+  }
+  calc_drift <- function(procdata){
+    # format the data for uncertainty calculation
+	# first row is mean, second is sd
+	times <- seq(max(procdata$startbasevolt$timestamp), 
+	             max(procdata$endbasevolt$timestamp), by=15)
+	tsteps <- length(times)
+	calcdat <- data.frame(endbasevolt=c(mean(procdata$endbasevolt$SEmV), 
+	                                    sd(procdata$endbasevolt$SEmV)),
+						  startbasevolt=c(mean(procdata$startbasevolt$SEmV), 
+	                                      sd(procdata$startbasevolt$SEmV)),
+						  runtime=c(tsteps*15, 0))
+	equation <- expression((endbasevolt - startbasevolt)/runtime)
+	# intermediate data
+	intdat <- calc_with_uncertainty(equation, calcdat)
+	# calculate the change in baseline voltage with uncertainty
+	avg <- 15*seq(0, length(times)-1)*intdat$avg
+	stdev <- rep(intdat$stdev, length(avg)) ## correct?
+	return(data.frame(timestamp=times, avg=avg, stdev=stdev))
   }
   correct_drift <- function(d, drift){
     # find segment of drift that corresponds to timestamps in d
     sidx <- match(d$timestamp[1], drift$timestamp)
     eidx <- match(d$timestamp[nrow(d)], drift$timestamp)
-    d['driftmV'] <- drift$driftmV[seq(sidx, eidx)]
-    d['CSEmV'] <- d$SEmV - d$driftmV
+	interval <- seq(sidx, eidx)
+	corrected <- NULL
+	expr <- expression(SEmV - driftmV)
+	for(i in seq(along=interval)){
+	  calcdat <- data.frame(SEmV=c(d$SEmV[i], 0), # In this case SEmV is an exact measurement
+	                        driftmV=c(drift$avg[interval[i]], drift$stdev[interval[i]]))
+	  corrected <- rbind(corrected, calc_with_uncertainty(expr, calcdat))
+	}
+	d['CSEmV'] <- corrected$avg
+	d['CSEmV.sd'] <- corrected$stdev
     return(d)
   }
   summarize_basevolt <- function(startvolt, endvolt){
@@ -50,41 +80,83 @@ analyze_fogrun <- function(procdata, calibrationstandard=data.frame(avg=5.33, st
     # measurements to understand the overall variation (error) in 
     # load cell readings
     list(avg=mean(c(startvolt$CSEmV, endvolt$CSEmV)),
-         stdev=sd(c(startvolt$CSEmV, endvolt$CSEmV))
-    )
+         stdev=sd(c(startvolt$CSEmV, endvolt$CSEmV)))
   }  
   strip_basevoltage <- function(d, basevoltstats){
     # adds a column representing the load cell measurement stripped of
     # the offset voltage when the cradle is attached (the 'baseline' or
     # 'background' voltage)
-    d['loadmV.avg'] <- d$CSEmV - basevoltstats$avg
-    d['loadmV.stderr'] <- 2*basevoltstats$stdev
+	expr <- expression(CSEmV - basevolt)
+	stripped <- NULL
+	for(i in seq(along=d$CSEmV)){
+	  calcdat <- data.frame(CSEmV=c(d$CSEmV[i], d$CSEmV.sd[i]),
+	                        basevolt=c(basevoltstats$avg, basevoltstats$stdev))
+	  stripped <- rbind(stripped, calc_with_uncertainty(expr, calcdat))
+	}
+    d['loadmV'] <- stripped$avg
+    d['loadmV.sd'] <- stripped$stdev
     return(d)
-  }
+  }  
   calc_conversionfactor <- function(calibdat, refmass){
-    #calib <- propagate(expression(loadmV))
-    meancalib <- mean(calibdat$loadmV.avg)
-    stderrcalib <- mean(calibdat$loadmV.stderr)
-    list(avg=refmass$avg/meancalib, stderr=stderrcalib)
+    expr <- expression(refmass/calibdat)
+	# loadmV.sd is constant
+	cdstats <- stats_with_uncertainty(calibdat[c('loadmV', 'loadmV.sd')])
+	calcdat <- data.frame(refmass=c(refmass$avg, refmass$stdev),
+	                      calibdat=c(cdstats$avg, cdstats$stdev))
+	return(calc_with_uncertainty(expr, calcdat))
   }
   calc_watermass <- function(wetdat, drydat, convdat){
     # wetdat is e.g. procdat$wetragvolt
     # drydat would then be procdat$dryragvolt
     # convdat is data for the conversion factor
-    avgmass <- (mean(wetdat$loadmV.avg) - mean(drydat$loadmV.avg))*convdat$avg
-    stderrmass <- (mean(wetdat$loadmV.stderr) + 
-                   mean(drydat$loadmV.stderr))*convdat$loadmV.stderr
-    list(avg=avgmass, stderr=stderrmass)
+	expr <- expression((wetmass - drymass)*cf)
+	wrstats <- stats_with_uncertainty(wetdat[c('loadmV', 'loadmV.sd')])
+	drstats <- stats_with_uncertainty(drydat[c('loadmV', 'loadmV.sd')])
+	calcdat <- data.frame(wetmass=c(wrstats$avg, wrstats$stdev),
+	                      drymass=c(drstats$avg, drstats$stdev),
+						  cf=c(convdat$avg, convdat$stdev))
+    return(calc_with_uncertainty(expr, calcdat))
+  }
+  calc_dryleafmass <- function(d, convdat){
+    # d is the dry leaf readings
+	dlstats <- stats_with_uncertainty(procdata$dryleafvolt[c('loadmV', 'loadmV.sd')])
+	expr <- expression(leafvolt*cf)
+	calcdat <- data.frame(leafvolt=c(dlstats$avg, dlstats$stdev), 
+	                       cf=c(convdat$avg, convdat$stdev))
+	return(calc_with_uncertainty(expr, calcdat))
+  }
+  calc_leafwatermass <- function(leafdat, dlstats, convdat){
+    # leafdat is leaf of variable wetness, i.e. rundata
+	# dlstats is the dry leaf stats via stats_with_uncertainty()
+	expr <- expression((wetleaf - dryleaf)*cf)
+    m <- NULL			  
+	for(i in seq(along=leafdat$loadmV)){
+	  calcdat <- data.frame(wetleaf=c(leafdat$loadmV[i], leafdat$loadmV.sd[i]),
+							dryleaf=c(dlstats$avg, dlstats$stdev),
+                            cf=c(convdat$avg, convdat$stdev))
+	  m <- rbind(m, calc_with_uncertainty(expr, calcdat))
+	}
+	leafdat['watermass'] <- m$avg
+	leafdat['watermass.sd'] <- m$stdev
+	return(leafdat)
+  }
+  calc_surfacedensity <- function(leafdat, leafarea){
+    expr <- expression(watermass/leafarea)
+	m <- NULL
+	for(i in seq(along=leafdat$watermass)){
+	calcdat <- data.frame(leafarea=c(leafarea, 0), 
+	                      watermass=c(leafdat$watermass[i], 
+						              leafdat$watermass.sd[i]))
+	m <- rbind(m, calc_with_uncertainty(expr, calcdat))
+	}
+	leafdat[['surfacedensity']] <- m$avg
+	leafdat[['surfacedensity.sd']] <- m$stdev
   }
 ###
   # get species
   procdata[['species']] <- get_species(procdata$name)
-  # get base voltage mean and stdev
-  sbv <- summarize_load(procdata$startbasevolt)
-  ebv <- summarize_load(procdata$endbasevolt)
-  # calculate the base voltage drift assuming a linear change
-  # (suggested by Campbell Scientific)
-  measdrift <- calc_drift(sbv, ebv)
+  # calculate the drift
+  measdrift <- calc_drift(procdata)
   # correct procdata for drift
   # assumes startbasevolt is the earliest set of measurements
   # and endbasevolt is the latest set of measurements
@@ -93,24 +165,27 @@ analyze_fogrun <- function(procdata, calibrationstandard=data.frame(avg=5.33, st
     procdata[[label]] <- correct_drift(procdata[[label]], measdrift)
   # also add a dummy column to startbasevolt
   procdata$startbasevolt['CSEmV'] <- procdata$startbasevolt$SEmV
-  # get summarize basevolt to understand variability in baseline voltage
+  # get baseline voltage
   bvstats <- summarize_basevolt(procdata$startbasevolt, procdata$endbasevolt)
-  # strip the baseline voltage from everything
+  # strip baseline voltage from everything
   for(label in c('startcalibvolt', 'dryragvolt', 'wetragvolt', 'rundata', 
                  'dryleafvolt', 'startbasevolt', 'endbasevolt'))
     procdata[[label]] <- strip_basevoltage(procdata[[label]], bvstats)
   # get conversion factor from calibration data
-  cf <- calc_conversionfactor(procdata$startcalibvolt, calibrationstandard)
-  # calculate water mass on leaf and LWS
-  lwswm <- calc_watermass(procdata$wetragvolt, procdata$dryragvolt, cf)
-  # calculate water mass for rundata entries
-  procdata$rundata['watermass'] <- calc_watermass(procdata$rundata, 
-                                                  procdata$dryleafvolt, cf)
-  # also normalize by leaf area (areal or surface density, g/cm^2)
-  procdata$rundata <- get_surfacedensity(procdata$rundata, procdata$leafarea)
-  # add results of the analysis
-  procdata[['LWSwater']] <- lwswm
-  #procdata[['leafwater']] <- lwm
-  procdata[['conversion']] <- cf
+  procdata[['conversionfactor']] <- calc_conversionfactor(procdata$startcalibvolt, 
+                                                          calibrationstandard)
+  # calculate LWS water mass
+  procdata[['LWSwatermass']] <- calc_watermass(procdata$wetragvolt, 
+                                             procdata$dryragvolt, 
+											 procdata$conversionfactor)
+  # calculate leaf water mass for rundata
+  procdata[['dryleafmass']] <- calc_dryleafmass(procdata$dryleafvolt,
+											    procdata$conversionfactor)
+  procdata[['rundata']] <- calc_leafwatermass(procdata$rundata, 
+                                              procdata$dryleafmass,
+											  procdata$conversionfactor)
+  # normalize leaf mass by leaf area
+  procdata[['rundata']] <- calc_surfacedensity(procdata$rundata, 
+                                               procdata$leafarea)
   return(procdata)
 }
